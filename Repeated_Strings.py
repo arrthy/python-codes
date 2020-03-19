1e3298cf03d8b19e7d24cc387dd21c64f58b3c38
arrthy <41236583+arrthy@users.noreply.github.com>
2020-03-19 18:11:55 +0530

    Create Repeated_Strings.py

---
+++ Repeated_Strings.py
@@ -0,0 +1,19 @@
+def repeatedString(s, n):
+    c1 = s.count("a")
+    c2 = c1*(n//len(s))
+
+    c2 += s[:n % len(s)].count("a")
+    return c2
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
